652a1547a2e1a1fcaede44ab95f45dbcb19572af
Colby Fayock <fayock@gmail.com>
2015-09-20 14:10:43 -0400

    starting to customize to my liking and cleaning

--- brew.sh
+++ brew.sh
@@ -16,8 +16,8 @@ brew upgrade --all
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# brew install coreutils
+# sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -28,9 +28,9 @@ brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+# brew install bash
+# brew tap homebrew/versions
+# brew install bash-completion2
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -38,15 +38,15 @@ brew install wget --with-iri
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+# brew install ringojs
+# brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 #brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
 #brew tap bramstein/webfonttools
@@ -84,10 +84,10 @@ brew install homebrew/php/php55 --with-gmp
 #brew install ack
 #brew install dark-mode
 #brew install exiv2
-brew install git
+#brew install git
 #brew install git-lfs
 #brew install imagemagick --with-webp
-brew install lua
+#brew install lua
 #brew install lynx
 #brew install p7zip
 #brew install pigz
@@ -96,7 +96,7 @@ brew install lua
 #brew install rhino
 #brew install speedtest_cli
 #brew install ssh-copy-id
-brew install tree
+#brew install tree
 #brew install webkit2png
 #brew install zopfli
 
